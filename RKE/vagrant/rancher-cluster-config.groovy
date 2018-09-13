6f317da830b940217e2414d342e7b4ec831aac50
developerSid <developerSid@gmail.com>
2018-09-12 21:25:15 -0500

    Began custom configuration of the rke based install

--- RKE/vagrant/rancher-cluster-config.groovy
+++ RKE/vagrant/rancher-cluster-config.groovy
@@ -1,2 +1,11 @@
-import groovy.text.StreamingTemplateEngine
+@Grapes(
+    @Grab(group='commons-codec', module='commons-codec', version='1.11')
+)
 
+import groovy.text.StreamingTemplateEngine
+import org.apache.commons.codec.binary.Base64
+
+def caCert = Base64.encodeBase64String(new File('../../Certificates/vagrant/out/ca.pem').getBytes())
+def caCertKey = Base64.encodeBase64String(new File('../../Certificates/vagrant/out/ca-key.pem').getBytes())
+def masterCert = Base64.encodeBase64String(new File('../../Certificates/vagrant/out/master.vagrant.example.pem').getBytes())
+def masterCertKey = Base64.encodeBase64String(new File('../../Certificates/vagrant/out/master.vagrant.example-key.pem').getBytes())
