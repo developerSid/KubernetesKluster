c2ddf69792589e509cefe9182c12cccab6ab6ae7
developerSid <developerSid@gmail.com>
2018-09-12 23:46:33 -0500

    Added more specialized configuration to the rke based cluster via the rancher-cluster.yml and rancher-cluster-config.groovy

--- RKE/vagrant/rancher-cluster-config.groovy
+++ RKE/vagrant/rancher-cluster-config.groovy
@@ -1,11 +1,35 @@
-@Grapes(
-    @Grab(group='commons-codec', module='commons-codec', version='1.11')
-)
+@Grapes([
+    @Grab(group='commons-codec', module='commons-codec', version='1.11'),
+    @Grab(group='commons-io', module='commons-io', version='2.6')
+])
 
 import groovy.text.StreamingTemplateEngine
+import org.apache.commons.io.*
 import org.apache.commons.codec.binary.Base64
+import java.nio.file.Paths
 
-def caCert = Base64.encodeBase64String(new File('../../Certificates/vagrant/out/ca.pem').getBytes())
-def caCertKey = Base64.encodeBase64String(new File('../../Certificates/vagrant/out/ca-key.pem').getBytes())
-def masterCert = Base64.encodeBase64String(new File('../../Certificates/vagrant/out/master.vagrant.example.pem').getBytes())
-def masterCertKey = Base64.encodeBase64String(new File('../../Certificates/vagrant/out/master.vagrant.example-key.pem').getBytes())
+def baseProjectDir = FilenameUtils.normalize(new File('../../').getAbsolutePath())
+def outDir = new File('./out')
+
+if (outDir.exists()) {
+   FileUtils.forceDelete(outDir)
+}
+
+outDir.mkdir()
+
+def caCert = Base64.encodeBase64String(new File("$baseProjectDir/Certificates/vagrant/out/ca.pem").getBytes())
+def masterCert = Base64.encodeBase64String(new File("$baseProjectDir/Certificates/vagrant/out/master.vagrant.example.pem").getBytes())
+def masterCertKey = Base64.encodeBase64String(new File("$baseProjectDir/Certificates/vagrant/out/master.vagrant.example-key.pem").getBytes())
+
+def transformedTemplate = new File('./in/rancher-cluster.yml').withReader { reader ->
+   def template = new StreamingTemplateEngine().createTemplate(reader)
+   
+   return template.make([
+      'baseProjectDir': baseProjectDir,
+      'caCert': caCert,
+      'masterCert': masterCert,
+      'masterCertKey': masterCertKey
+   ])
+}
+
+new File(outDir, 'cluster.yml').append(transformedTemplate)
